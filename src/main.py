cef1b704d11ed0ab1ce39cab6557442edb4d269c
Minh Hoang Nguyen <68138084+mhngu23@users.noreply.github.com>
2022-05-30 17:22:34 +1000

    Final Version Of Traffic SIgn Image Recognition online python
    * Delete not important file
    * Add final run of the Traffic Sign Image Recognition FIle

--- src/main.py
+++
@@ -1,8 +0,0 @@
-from datasetReader import DatasetReader
-
-if __name__ == '__main__':
-    image_folder_path = "..\\trafficsigns_dataset"
-    imageDataReader = DatasetReader(image_folder_path)
-    imageDataReader.create_shape_dataset()
-    print(len(imageDataReader.shapeImageFeatures))
-    print(len(imageDataReader.shapeClassName))
